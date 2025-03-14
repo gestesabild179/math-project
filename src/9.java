81527778578cd5b6dfc7834a588d57c3e9db49a0
gestesabild179 <gestesabild179@gmail.com>
2025-03-14 07:48:16 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,7 @@
+public class MathProject {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
