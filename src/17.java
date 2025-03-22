b1f36d641322f0de62e86c05c6f08b734dccdda8
gestesabild179 <gestesabild179@gmail.com>
2025-03-22 16:47:01 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,23 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("Addition: " + (10 + 5));
+        System.out.println("Subtraction: " + (10 - 5));
+        System.out.println("Multiplication: " + (10 * 5));
+        System.out.println("Division: " + (10 / 5));
+
+        // Example of loop operations
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        // Example of conditional logic
+        if (true) {
+            System.out.println("Condition true");
+        } else {
+            System.out.println("Condition false");
+        }
+    }
+}
