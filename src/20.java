055f32eaa41b903fe0705c5872be39b702c1b7aa
gestesabild179 <gestesabild179@gmail.com>
2025-03-29 19:24:22 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,28 @@
+public class MathProject {
+    // Example of some Java code related to mathematics
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x = 5;
+        double y = 3.14;
+        float z = 2.7f;
+        
+        // Add, subtract, multiply, divide, and compare these numbers
+        double sum = x + y;
+        double difference = y - x;
+        double product = x * y;
+        double quotient = y / x;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        
+        // Calculate the area and circumference of a circle
+        double radius = 7;
+        double pi = Math.PI;
+        double area = pi * radius * radius;
+        double circumference = 2 * pi * radius;
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+    }
+}
