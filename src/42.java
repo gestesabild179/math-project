05800d3815ea8bd9f6b0c2a57ff849d7d350195d
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 12:48:56 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,10 @@
+public class MathProject {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        
+        // Add two numbers
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
