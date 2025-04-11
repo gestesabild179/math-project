ae6a0c088c3f54859bdc7a37287250dd308ae7fb
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 12:54:39 +0000

    Add 45.java

---
+++ src/45.java
@@ -0,0 +1,12 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example code for simple calculations
+        System.out.println("Hello, world!");
+        int x = 5;
+        double y = 3.14;
+
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+    }
+}
