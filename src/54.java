2ea23f1652b3e1a18007a6d9d9b3deee9993141a
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 14:10:52 +0000

    Add 54.java

---
+++ src/54.java
@@ -0,0 +1,9 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("2 + 3 = " + (2 + 3));
+        System.out.println("10 - 5 = " + (10 - 5));
+        System.out.println("4 * 2 = " + (4 * 2));
+        System.out.println("5 / 2 = " + (5 / 2));
+    }
+}
