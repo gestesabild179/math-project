37319a7f8b0fdf643c7e5c28e79aed3d9920828b
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 12:39:07 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,12 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example code to calculate simple addition and subtraction
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        System.out.println("The result of " + num1 + " + " + num2 + " is: " + sum);
+        System.out.println("The result of " + num1 + " - " + num2 + " is: " + difference);
+    }
+}
