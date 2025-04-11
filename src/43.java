69ce111d3bd0d445858a1381cf9ad88b80786fb4
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 12:50:33 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,10 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of performing basic arithmetic operations
+        int result = 4 + 2;
+        System.out.println("The sum is: " + result);
+        
+        double result2 = 8 / 2;
+        System.out.println("The division is: " + result2);
+    }
+}
