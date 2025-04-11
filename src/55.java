56981363e5cd1518caf876356f7e8b1fd5525d35
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 14:13:17 +0000

    Add 55.java

---
+++ src/55.java
@@ -0,0 +1,18 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of an arithmetic expression using Java's built-in operators
+        System.out.println("5 + 3 = " + (5 + 3));
+        
+        // Example of a mathematical operation with variables
+        double result1 = 2.0 * 4.5;
+        System.out.println("Result of 2 * 4.5 is: " + result1);
+        
+        // Example of performing a simple calculation using an if statement
+        boolean evenNumber = true;
+        if (evenNumber) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
