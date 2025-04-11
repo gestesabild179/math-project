084ef7a7d5426d800d123202ebd0fa59c3620095
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 12:59:32 +0000

    Add 48.java

---
+++ src/48.java
@@ -0,0 +1,9 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of using Java to perform some arithmetic operations
+        int a = 5;
+        int b = 3;
+        System.out.println("The sum is: " + (a + b));
+        System.out.println("The difference is: " + (a - b));
+    }
+}
