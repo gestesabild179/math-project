e128b13e7ea26e22179c8f54844ed2f3bb8376b1
gestesabild179 <gestesabild179@gmail.com>
2025-04-11 14:07:20 +0000

    Add 52.java

---
+++ src/52.java
@@ -0,0 +1,21 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example 1: Addition of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum is: " + (num1 + num2));
+
+        // Example 2: Subtraction of two numbers
+        double result = 9 - 4.7;
+        System.out.println("Result: " + result);
+
+        // Example 3: Multiplication of two numbers
+        int product = 6 * 5;
+        System.out.println("Product is: " + product);
+
+        // Example 4: Division of two numbers (casting a long to an integer)
+        long dividend = 123L;
+        long divisor = -99L;
+        System.out.println("Division result is: " + (long) dividend / divisor);
+    }
+}
