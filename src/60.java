9b14a302de06f1a8bc084e62e3a662431443d4cf
gestesabild179 <gestesabild179@gmail.com>
2025-04-25 19:30:30 +0000

    Add 60.java

---
+++ src/60.java
@@ -0,0 +1,12 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of a simple arithmetic operation
+        int result = 5 + 3;
+        System.out.println("The sum is: " + result);
+        
+        // Example of accessing an array element
+        int[] numbers = {1, 2, 3, 4};
+        int elementIndex = 2;
+        System.out.println("Element at index " + elementIndex + ": " + numbers[elementIndex]);
+    }
+}
