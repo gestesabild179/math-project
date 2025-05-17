f861bf5fbd471b2b056b58ae2da708241a7ff918
gestesabild179 <gestesabild179@gmail.com>
2025-05-17 06:26:50 +0000

    Add 63.java

---
+++ src/63.java
@@ -0,0 +1,7 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example mathematical operations and calculations
+        double result = 2 + 3 * 4 - 5;
+        System.out.println("The result is: " + result);
+    }
+}
