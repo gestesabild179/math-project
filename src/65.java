8b81b0dfa7dfe3f4f62497780d08771de02d9900
gestesabild179 <gestesabild179@gmail.com>
2025-05-19 15:50:34 +0000

    Add 65.java

---
+++ src/65.java
@@ -0,0 +1,7 @@
+public class MathProject {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 3.14;
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+    }
+}
