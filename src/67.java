b1a0856f8dcc562c24dc113b4b6763cfa15d3fca
gestesabild179 <gestesabild179@gmail.com>
2025-05-23 06:56:01 +0000

    Add 67.java

---
+++ src/67.java
@@ -0,0 +1,8 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
