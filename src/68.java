0384b7df5eeff532108e8f44ea061c669596c796
gestesabild179 <gestesabild179@gmail.com>
2025-05-24 04:52:11 +0000

    Add 68.java

---
+++ src/68.java
@@ -0,0 +1,9 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example of calculating the area of a rectangle
+        double length = 5.0;
+        double width = 3.0;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
