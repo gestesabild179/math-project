29e5e8f923d801eeae501e45851a7d2f10926fca
gestesabild179 <gestesabild179@gmail.com>
2025-05-26 16:03:51 +0000

    Add 70.java

---
+++ src/70.java
@@ -0,0 +1,7 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example code to perform some mathematical operations
+        int result = 5 + 3 * 2 - 4 / 2;
+        System.out.println("Result: " + result);
+    }
+}
